ad46ed71a09c95cb00f21d3f1b4ca1d8069b0ba0
Mohamed <mwarith25@gmail.com>
2023-12-14 23:43:08 +0200

    full

--- lib/whatsapp.dart
+++ lib/whatsapp.dart
@@ -15,46 +15,199 @@ class WhatsApp extends StatelessWidget {
         ),
         actions: const [
           Icon(Icons.search),
-          Padding(
-            padding: EdgeInsets.only(right: 10),
+          SizedBox(
+            width: 10,
           ),
           Icon(Icons.more_vert),
-          Padding(
-            padding: EdgeInsets.only(right: 10),
+          SizedBox(
+            width: 10,
           ),
         ],
         elevation: 0,
       ),
-      body: Container(
-        color: const Color(0xff4ca047),
-        height: 45,
-        child: const Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'CHAT',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.white,
-              ),
+      body: Column(
+        children: [
+          Container(
+            color: const Color(0xff4ca047),
+            height: 50,
+            child: const Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  'CHAT',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'STATUS',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'CALLS',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'STATUS',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.white,
+          ),
+          const SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 10,
               ),
-            ),
-            Text(
-              'CALLS',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.white,
+              Icon(
+                Icons.person,
+                size: 70,
               ),
-            ),
-          ],
-        ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Mohamed sayed',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'hello my friend...',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.person,
+                size: 70,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Ronaldo',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'حجز الساعة 5 جاي ؟',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.person,
+                size: 70,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'john',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'where are you ?',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.person,
+                size: 70,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'سرج',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      height: 1,
+                    ),
+                  ),
+                  Text(
+                    'يا انستراكتور',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
